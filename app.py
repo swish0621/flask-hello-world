a52247fa63af3fa7a5b81bd0493223021342329a
swish0621 <swish0621@gmail.com>
2025-07-29 09:59:11 -0600

    fix routes'/'

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World From Nicholas Swisher in 3308.'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test(): 
     conn = psycopg.connect("postgresql://lab10_database_l65b_user:6u5YCsT039jmaSE0knSRY6002qOUTPAP@dpg-d24e42ngi27c73dgfulg-a/lab10_database_l65b")
     conn.close()
     return "Database connection successful!"
 
-@app.route('create_db')
+@app.route('/create_db')
 def create_db():
     conn = psycopg.connect("postgresql://lab10_database_l65b_user:6u5YCsT039jmaSE0knSRY6002qOUTPAP@dpg-d24e42ngi27c73dgfulg-a/lab10_database_l65b")
     cur = conn.cursor()
